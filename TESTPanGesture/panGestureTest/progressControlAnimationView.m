53e1dbe2619ed12a176adfe35065df6872177a39
jimmy <jimmy.mao@hex-tech.net>
2021-09-01 14:38:20 +0800

    优化取消动画操作

--- TESTPanGesture/panGestureTest/progressControlAnimationView.m
+++ TESTPanGesture/panGestureTest/progressControlAnimationView.m
@@ -19,7 +19,7 @@
 @interface progressControlAnimationView ()
 @property (nonatomic, strong) UIView *animationView;
 @property (nonatomic, strong) UILabel *statusLabel;
-@property (nonatomic, assign) CGRect animationBeforeFrame;
+@property (nonatomic, strong) CADisplayLink *displayLink;
 @end
 
 @implementation progressControlAnimationView
@@ -29,9 +29,12 @@
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor colorWithWhite:0 alpha:0.5];
+        
+        
         [self addSubview:self.animationView];
         [self.animationView addSubview:self.statusLabel];
         [self addPanGesture];
+        
     }
     return self;
 }
@@ -48,29 +51,36 @@
     progressY = progressY * (screenH / animationViewShowH);
     
     if (recognizer.state == UIGestureRecognizerStateBegan) {
+        
+        self.layer.beginTime = 0;
+        self.layer.timeOffset = 0;
+        self.layer.speed = 0;
+        self.layer.repeatCount = INT_MAX;
+        
         //判断滑动方式：横向or纵向
         CGPoint direction = [recognizer velocityInView:self];
-        
-        //上滑
-        if (direction.y < 0) {
-            [self moveUpAnimation];
+                    
+        [UIView animateKeyframesWithDuration:animationDuration delay:0 options:UIViewKeyframeAnimationOptionAllowUserInteraction animations:^{
             
-        }else{
-            [self moveDownAnimation];
-        }
+            if (direction.y > 0) {
+                self.animationView.frame = CGRectMake(0, screenH-animationViewShowH, screenW, animationViewH);
+            }else{
+                self.animationView.frame = CGRectMake(0, screenH-animationViewH, screenW, animationViewH);
+            }
+        } completion:nil];
         
+
     }else if (recognizer.state == UIGestureRecognizerStateChanged){
         
-        
-        if (progressY > 0.3) {
+        if (progressY > 0.5) {
             self.statusLabel.text = @"松手自动继续动画";
         }else{
             self.statusLabel.text = [NSString stringWithFormat:@"滑动进度=%f",progressY];
         }
-    
+
         [self animationProgress:progressY];
     }else if (recognizer.state == UIGestureRecognizerStateEnded || recognizer.state == UIGestureRecognizerStateCancelled){
-        if (progressY > 0.3) {
+        if (progressY > 0.5) {
             //完成剩余动画
             [self finishAnimation];
         }else{
@@ -79,42 +89,6 @@
         }
     }
 }
-
--(void)moveUpAnimation{
-    
-    //还原之前动画对象的时间
-    self.animationView.layer.beginTime = 0;
-    self.animationView.layer.timeOffset = 0;
-    
-    self.animationBeforeFrame = self.animationView.frame;
-
-    [UIView animateWithDuration:animationDuration animations:^{
-        
-        self.animationView.frame = CGRectMake(0, screenH-animationViewH, screenW, animationViewH);
-        
-    } completion:^(BOOL finished) {
-        
-    }];
-    
-    self.animationView.layer.speed = 0;
-
-}
-
--(void)moveDownAnimation{
-    
-    //还原之前动画对象的时间
-    self.animationView.layer.beginTime = 0;
-    self.animationView.layer.timeOffset = 0;
-    
-    self.animationBeforeFrame = self.animationView.frame;
-    
-    [UIView animateWithDuration:animationDuration animations:^{
-        self.animationView.frame = CGRectMake(0, screenH-animationViewShowH, screenW, animationViewH);
-    }];
-    
-    self.animationView.layer.speed = 0;
-}
-
 
 /**
  对比初始动画speed=1的区别：
@@ -128,7 +102,7 @@
 -(void)animationProgress:(CGFloat )p{
     
     //该对象的本地时间timeOffset,它是相对于本地时间0s的一个偏移
-    self.animationView.layer.timeOffset = p*animationDuration;
+    self.layer.timeOffset = p*animationDuration;
     
 }
 
@@ -144,27 +118,37 @@ begin - beginTime
 -(void)finishAnimation{
     
     //1、之前滑动时动画的最后执行时间
-    CFTimeInterval du = self.animationView.layer.timeOffset;
+    CFTimeInterval pausedTime = self.layer.timeOffset;
         
     //2、将timeOffset还原（动画执行前设置的timeOffset的值，我这里之前没设置所以默认为0）
-    self.animationView.layer.timeOffset = 0;
+    self.layer.timeOffset = 0;
     
     //3、使layer对象的动画进度受到父对象本地时间的影响
-    self.animationView.layer.speed = 1;
+    self.layer.speed = 1;
     
     //4、因为speed置为1后，layer对象的beginTime受到父对象本地时间的影响
     //所以layer对象动画执行时间为：t = (tp - begin) * speed + timeOffset，计算出layer动画对象的本地时间为:du（即手势结束动画时的time）
-    self.animationView.layer.beginTime = [self.animationView.layer convertTime:CACurrentMediaTime() toLayer:nil] - du;
+    self.layer.beginTime = [self.layer convertTime:CACurrentMediaTime() fromLayer:nil] - pausedTime;
 }
 
 //取消之前执行的进度动画
 -(void)cancelAnimation{
     
-    //移除动画
-    [self.animationView.layer removeAllAnimations];
+    self.displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(cancelLink)];
+    [self.displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
 
-    //虽然移除了动画，但是动画block代码仍旧会执行，所以需要还原view frame
-    self.animationView.frame = self.animationBeforeFrame;
+}
+
+-(void)cancelLink{
+    
+    CFTimeInterval duration = [self.displayLink duration];
+    NSTimeInterval timeOffset = self.layer.timeOffset-duration;
+    
+    if (timeOffset < 0) {
+        [self.displayLink invalidate];
+    }else{
+        self.layer.timeOffset = timeOffset;
+    }
 }
 
 
